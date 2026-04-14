4a48890ff97639bfc38e054e7083ccb562e0e755
D4rkst3r <az3198284@gmail.com>
2026-04-14 21:49:16 +0200

    Korrigierte Knochen-IDs und -namen in der Konfiguration; aktualisierte Anhänge für die Kettensäge

--- config.lua
+++ config.lua
@@ -30,17 +30,17 @@ Config.Animations = {
 }
 
 Config.Bones = {
-    { name = 'SKEL_R_Hand',     id = 28422 },
-    { name = 'SKEL_L_Hand',     id = 57005 },
-    { name = 'SKEL_R_Forearm',  id = 61007 },
-    { name = 'SKEL_L_Forearm',  id = 63931 },
-    { name = 'SKEL_Spine2',     id = 24818 },
-    { name = 'SKEL_R_UpperArm', id = 40269 },
-    { name = 'SKEL_L_UpperArm', id = 45509 },
-    { name = 'IK_R_Hand',       id = 36029 },
-    { name = 'IK_L_Hand',       id = 65245 },
-    { name = 'PH_R_Hand',       id = 26613 },
-    { name = 'PH_L_Hand',       id = 18905 },
-    { name = 'Head',            id = 31086 },
-    { name = 'Pelvis',          id = 11816 },
+    { name = 'SKEL_R_Hand',     id = 57005 }, -- Korrigiert (war 28422 -> PH_R_Hand)
+    { name = 'SKEL_L_Hand',     id = 18905 }, -- Korrigiert (war 57005 -> SKEL_R_Hand)
+    { name = 'SKEL_R_Forearm',  id = 28252 }, -- Korrigiert (war 61007 -> RB_L_ForeArmRoll)
+    { name = 'SKEL_L_Forearm',  id = 61163 }, -- Korrigiert (war 63931 -> SKEL_L_Calf / linke Wade)
+    { name = 'SKEL_Spine2',     id = 24817 }, -- Korrigiert (war 24818 -> SKEL_Spine3)
+    { name = 'SKEL_R_UpperArm', id = 40269 }, -- War richtig!
+    { name = 'SKEL_L_UpperArm', id = 45509 }, -- War richtig!
+    { name = 'IK_R_Hand',       id = 6286 },  -- Korrigiert (war 36029 -> IK_L_Hand)
+    { name = 'IK_L_Hand',       id = 36029 }, -- Korrigiert (war 65245 -> IK_L_Foot)
+    { name = 'PH_R_Hand',       id = 28422 }, -- Korrigiert (war 26613 -> SKEL_L_Finger30)
+    { name = 'PH_L_Hand',       id = 60309 }, -- Korrigiert (war 18905 -> SKEL_L_Hand)
+    { name = 'SKEL_Head',       id = 31086 }, -- ID war richtig (31086), Name als 'SKEL_Head' ist sauberer
+    { name = 'SKEL_Pelvis',     id = 11816 }, -- ID war richtig (11816), Name als 'SKEL_Pelvis' ist sauberer
 }
